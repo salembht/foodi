e628f0184f9ac5c23eed47d48ea663c6e62aff43
salembht <127092764+salembht@users.noreply.github.com>
2023-10-14 10:50:16 +0300

    make the functio of slider

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,1 +1,18 @@
-document.getElementsByClassName("dish-cards")
+const cardsContainer = document.getElementsByClassName("inner")[0];
+const cards = document.getElementsByClassName("card");
+let size = cards.length ;
+let index =0;
+const message=document.getElementsByClassName("netfaction")
+message.style.display='none';
+function slidRight() {
+    if(index > -(size-2)*(100/size)){
+        index-=100/size;
+        cardsContainer.style.transform=`translateX(${index}%)`;
+    }
+  }
+function slidLeft() { 
+    if( index<0){
+        index+=100/size;
+        cardsContainer.style.transform=`translateX(${index}%)`;
+    }
+}
