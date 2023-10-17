e7748504ad68b36d1f5314d1abad74fa40dcd674
salembht <127092764+salembht@users.noreply.github.com>
2023-10-17 19:42:46 +0300

    complate the media quiry

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,8 +2,17 @@ const cardsContainer = document.getElementsByClassName("inner")[0];
 const cards = document.getElementsByClassName("card");
 let size = cards.length ;
 let index =0;
-const message=document.getElementsByClassName("netfaction")
-message.style.display='none';
+const navbutton=document.getElementById('collapesbtn')
+navbutton.addEventListener('click',(event)=>{
+    const content = document.getElementById('nav')
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      navbutton.style.border='1px solid black';
+    } else {
+      content.style.display = "block";
+      navbutton.style.border='1px solid black';
+    }
+})
 function slidRight() {
     if(index > -(size-2)*(100/size)){
         index-=100/size;
